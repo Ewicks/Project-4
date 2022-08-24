1cca92f9716f3f445d282f226b674327f2876b88
Ewicks <ethanowicks9@gmail.com>
2022-08-24 18:40:58 +0000

    Add about and contact pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="blog"),
-]
+    path("", views.PostList.as_view(), name="index"),
+    path("blog/", views.PostList.as_view(), name="blog"),
+    path("about/", views.PostList.as_view(), name="about"),
+    path("contact/", views.PostList.as_view(), name="contact"),
+    ]
