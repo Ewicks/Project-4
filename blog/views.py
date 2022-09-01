f4074425dd6e72b5cc685f47792890e74d656cbf
Ewicks <ethanowicks9@gmail.com>
2022-09-01 16:10:27 +0000

    Connect urls

--- blog/views.py
+++ blog/views.py
@@ -10,13 +10,13 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 def about(request):
-    """ A view to return the index page """
+    """ A view to return the about page """
     return render(request, 'about.html')
 
 def contact(request):
-    """ A view to return the index page """
+    """ A view to return the contact page """
     return render(request, 'contact.html')
 
 def blog(request):
-    """ A view to return the index page """
+    """ A view to return the blog page """
     return render(request, 'blog.html')
