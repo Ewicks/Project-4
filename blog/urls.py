7d6f18b1a6a3b923d29af6425919124be0e9fd6d
Ewicks <ethanowicks9@gmail.com>
2022-09-05 15:52:37 +0000

    Add image,title to index.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="index"),
+    path("index/", views.PostList.as_view(), name="index"),
     path("blog/", views.blog, name="blog"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
