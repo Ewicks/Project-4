80b856b110eddd5709233ff578969a2522fb8f7c
Ewicks <ethanowicks9@gmail.com>
2022-09-07 19:43:22 +0000

    Add like functionality

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("add_post/", AddPostView.as_view(), name="add_post"),
     path("article/edit/<int:pk>", UpdatePostView.as_view(), name="update_post"),
     path("article/<int:pk>/delete", DeletePostView.as_view(), name="delete_post"),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
 
