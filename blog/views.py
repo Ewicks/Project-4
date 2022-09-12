5c4802945e47dc224e83697756eab448b620e7f1
Ewicks <ethanowicks9@gmail.com>
2022-09-12 12:12:37 +0000

    Add hover color for footer links

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Post
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from .forms import CommentForm
 
 
 class PostDetail(View):
@@ -10,53 +11,53 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        # comments = post.comments.filter(approved=True).order_by("-created_on")
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
             request,
-            "blog.html",
+            "article_details.html",
             {
                 "post": post,
-                # "comments": comments,
+                "comments": comments,
                 "commented": False,
                 "liked": liked,
-                # "comment_form": CommentForm()
+                "comment_form": CommentForm()
             },
         )
 
-def post(self, request, slug, *args, **kwargs):
+# def post(self, request, slug, *args, **kwargs):
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        # comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+#         queryset = Post.objects.filter(status=1)
+#         post = get_object_or_404(queryset, slug=slug)
+#         comments = post.comments.filter(approved=True).order_by("-created_on")
+#         liked = False
+#         if post.likes.filter(id=self.request.user.id).exists():
+#             liked = True
 
-        # comment_form = CommentForm(data=request.POST)
-        # if comment_form.is_valid():
-        #     comment_form.instance.email = request.user.email
-        #     comment_form.instance.name = request.user.username
-        #     comment = comment_form.save(commit=False)
-        #     comment.post = post
-        #     comment.save()
-        # else:
-        #     comment_form = CommentForm()
+#         comment_form = CommentForm(data=request.POST)
+#         if comment_form.is_valid():
+#             comment_form.instance.email = request.user.email
+#             comment_form.instance.name = request.user.username
+#             comment = comment_form.save(commit=False)
+#             comment.post = post
+#             comment.save()
+#         else:
+#             comment_form = CommentForm()
 
-        return render(
-            request,
-            "blog.html",
-            {
-                "post": post,
-                # "comments": comments,
-                # "commented": True,
-                # "comment_form": comment_form,
-                "liked": liked
-            },
-        )
+#         return render(
+#             request,
+#             "article_details.html",
+#             {
+#                 "post": post,
+#                 "comments": comments,
+#                 "commented": True,
+#                 "comment_form": comment_form,
+#                 "liked": liked
+#             },
+#         )
 
 class PostLike(View):
     
