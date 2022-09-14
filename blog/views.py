61d6cdd904ec5793e380c0d02ecb0a91e028e7cb
Ewicks <ethanowicks9@gmail.com>
2022-09-14 15:55:15 +0000

    Add topic a links href

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,10 @@ from .models import Post
 from .forms import CommentForm, PostForm
 
 
-
 def TopicView(request, tops):
     topics_posts = Post.objects.filter(topics=tops)
-    # print("topic posts: ", topic_posts)
-    return render(request, 'topics.html', {'tops': tops, 'topics_posts': topics_posts})
+    return render(request, 'topics.html', {
+        'tops': tops, 'topics_posts': topics_posts})
 
 
 class PostDetail(View):
